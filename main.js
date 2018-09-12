8d8f1448d3efd5fa70b75386ba281928b127ee5e
jxlpzqc <jxlpzqc@qq.com>
2018-09-13 06:45:11 +0800

    hand in task

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".row-right").animate({'width':'500'},1000);
+    console.log("我不太能明白navbar浮动在banner上方啥意思，就做成了滚动高度大于某个值变身固定在页面顶端的效果");
+    console.log("我不太能明白两栏布局，右边宽度在加载时候发生变化是啥意思，所以就做了页面加载完成宽度从0变成500px的效果");
+})
+
+function btnclick(){
+    alert("I extremly love neup!虽然写过的网页也蛮多的，但可能我的理解题目能力比较差劲，页面不符合题目意思，具体的问题打印到console里了，希望学长们能看看哦");
+    
+}
